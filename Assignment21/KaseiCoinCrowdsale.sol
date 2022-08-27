f62245a542fd9dd7c8a70c2196ddf7784e26a1d4
winniemei1 <16549392+winniemei1@users.noreply.github.com>
2022-08-27 14:15:08 -0400

    Assignment21 final edits

--- Assignment21/KaseiCoinCrowdsale.sol
+++ Assignment21/KaseiCoinCrowdsale.sol
@@ -8,38 +8,37 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
-    
-    // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale{
     constructor(
         uint256 rate,
         address payable wallet,
-        KaseiCoin token 
-    ) public Crowdsale(rate, wallet, token) {
-        // constructor can stay empty
+        KaseiCoin token
+    )
+    Crowdsale(rate,wallet,token)
+    public
+    {
+
+    } // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
     }
-}
-
-
-contract KaseiCoinCrowdsaleDeployer {
+    contract Kasei_Deployer {
     address public kasei_token_address;
     address public kasei_crowdsale_address;
-    
 
-    // Add the constructor.
+    // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
         string memory name,
         string memory symbol,
         address payable wallet
-    ) public {
-        // Create a new instance of the KaseiCoin contract.
+    
+    ) 
+    public
+    {
         KaseiCoin token = new KaseiCoin(name, symbol, 0);
         kasei_token_address = address(token);
-        
-        KaseiCoinCrowdsale kasei_crowdsale =
-            new KaseiCoinCrowdsale(1, wallet, token);
-        kasei_crowdsale_address = address(kasei_crowdsale);
-        
+
+        KaseiCoinCrowdsale kaseicrowdsale = new KaseiCoinCrowdsale(1, wallet, token);
+        kasei_crowdsale_address = address(kaseicrowdsale);
+
         token.addMinter(kasei_crowdsale_address);
         token.renounceMinter();
     }
